18a60939f32aa6a87993eff9bf086afe8ac92fff
Zietieflr <lrmcguire93@gmail.com>
2020-07-27 13:45:17 -0600

    Initialize React. Remove boilerplate from aapp and CSS. Initialize file structure. Add text to render to front page.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conditions</h1>
+      <h2>For Pathfinder 2e</h2>
+    </div>
+  );
+}
+
+export default App;
